1d5a48021e0198ccb90e8f016d9571b133093025
steve <steve@libertyapps.co.uk>
2025-12-11 15:10:33 +0000

    traffic light colours

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,3 +5,14 @@ const String apiKey = 'ZVUJN5EMPV';
 const Color primaryColor = Color(0xFF317CD3);
 const Color accentColor = Color(0xFF94529C);
 const Color editablePriceColor = Color(0xFF94ABBE);
+
+const Color trafficGreen = Color(0xFF6B8834);
+const Color trafficYellow = Color(0xFFF6EB14);
+const Color trafficRed = Color(0xFFD33131);
+const Color purple = Color(0xFF94529C);
+const Color purpleDark = Color(0xFF317CD3);
+const Color purpleLight = Color(0xFF83B1E8);
+const Color blue = Color(0xFF317CD3);
+const Color blueDark = Color(0xFF4A657C);
+const Color blueLight = Color(0xFF4A657C);
+const Color blueGrey = Color(0xFF94ABBE);
